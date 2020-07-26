706abc273653d47fd00ccf7f33e4083b57bc0656
pekca-fake92 <pekca-fake92@users.noreply.github.com>
2020-07-27 01:14:42 +0530

    Updates

---
+++ precache-manifest.da9117389d943fc5ca53623ad243eca2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b95ca2c5b99f9e591dc708857a500d3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3008272f3f1633ebe22f",
+    "url": "/static/css/main.a3ad1011.chunk.css"
+  },
+  {
+    "revision": "1bb5dd2a6657ff2ee685",
+    "url": "/static/js/2.b6f2604e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b6f2604e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3008272f3f1633ebe22f",
+    "url": "/static/js/main.e2c64b62.chunk.js"
+  },
+  {
+    "revision": "f64cd0e41d72611b1795",
+    "url": "/static/js/runtime-main.ee82bde8.js"
+  }
+]);
